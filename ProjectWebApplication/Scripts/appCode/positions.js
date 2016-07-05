7790603bc4f2dd25ff073a6e4515d141aa103581
Joel Nieto <joelnietotec@outlook.com>
2016-07-04 20:17:52 -0500

    asasas

--- ProjectWebApplication/Scripts/appCode/positions.js
+++ ProjectWebApplication/Scripts/appCode/positions.js
@@ -1,11 +1,28 @@
 ﻿var positionsController = function ($scope, $http) {
+    $scope.items = [];
+    $scope.newPosition = {};
+
     $http.get("http://localhost:24616/api/Positions")
         .success(function (data) {
             console.log(data);
+            $scope.items = data;
         })
         .error(function (err) {
             console.log(err.description);
         });
+    $scope.addPosition = function () {
+        $http.post("http://localhost:24616/api/Positions", {
+            Name: $scope.newPosition.Name,
+            Description: $scope.newPosition.Description
+        })
+        .success(function (data, status, headers, config) {
+            $scope.items.push($scope.newPosition);
+            $scope.addPosition = {};
+        })
+        .error(function (data, status, headers, config) {
+            console.log(error);
+        })
+    }
 }
 
 positionsController.$inject = ['$scope', '$http'];
